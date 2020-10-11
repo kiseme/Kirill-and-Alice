1023b668a60da03001fab67052d48d1907f6ed8d
Alice <alisa.4831@gmail.com>
2020-10-11 20:33:58 +0700

    complete lab

--- To-Do-List/src/app/Components/first/first.component.ts
+++ To-Do-List/src/app/Components/first/first.component.ts
@@ -3,16 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
-  styleUrls: ['./first.component.scss'],
-  template: `<ul>
-                  <li *ngFor="let item of items">{{item}}</li>
-                </ul>`  
+  styleUrls: ['./first.component.scss']  
 })
-export class FirstComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FirstComponent {
+  title = 'To Do List';
+  todos = [
+    {
+      name: 'Buy milk',
+      description: 'Go to shop and buy milk',
+      done: false
+    },
+    {
+      name: 'Buy bread',
+      description: 'Go to shop and buy bread',
+      done: false
+    }
+  ]
 }
