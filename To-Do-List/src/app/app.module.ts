b6238f0c32a4191372e55711bf5415451a5671e3
kiseme <semenyukkirill@gmail.com>
2020-10-18 22:02:36 +0700

    Всё ещё не получается

--- To-Do-List/src/app/app.module.ts
+++ To-Do-List/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './Components/first/first.component';
+import { ViewTodoComponent } from './Components/view-todo/view-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    ViewTodoComponent
   ],
   imports: [
     BrowserModule,
