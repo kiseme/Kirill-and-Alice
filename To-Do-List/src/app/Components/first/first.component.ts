f86ca26b3e646620a31e0c68da3f79e87bcd5ff9
kiseme <semenyukkirill@gmail.com>
2020-10-18 14:45:05 +0700

    Задание переделано с использованием сервиса

--- To-Do-List/src/app/Components/first/first.component.ts
+++ To-Do-List/src/app/Components/first/first.component.ts
@@ -5,13 +5,6 @@ import {TodosService} from './todos.service';
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.scss'],
-  template: `<div>
-            <table>
-            <tr *ngFor="let item of items">
-                <td>{{item}}</td>
-            </tr>
-        </table>
-    </div>`,
   providers: [TodosService]
 })
 export class FirstComponent {
